84ad9e9f66c859f265e84efd0ce761f8d81f645a
Odili <odiliejeh09@gmail.com>
2025-12-14 15:19:33 +0100

    assignmeent

---
+++ christmas/december task 1/christmas.js
@@ -0,0 +1,170 @@
+
+
+firstDay();
+secondDay();
+thirdDay();
+fourthDay();
+fifthDay();
+sixthDay();
+seventhDay();
+eigthDay();
+ninthDay();
+tenthDay();
+eleventhDay();
+twelfthDay();
+
+
+    function  firstDay(){
+        console.log("On the first day of christmas my true love sent to me");
+        firstDayBody();
+    }
+
+    function  firstDayBody(){
+        console.log("A patridge in a pear tree\n");
+    }
+
+     function  secondDay(){
+        console.log("On the second day of christmas my true love sent to me");
+        secondDayBody();
+    }
+
+    function  secondDayBody(){
+        console.log("Two turtle doves");
+        firstDayBody();
+    }
+
+     function  thirdDay(){
+        console.log("On the third day of christmas my true love sent to me");
+        thirdDayBody();
+    }
+
+    function  thirdDayBody(){
+        console.log("Three french hens");
+        secondDayBody();
+    }
+
+
+    function  fourthDay(){
+        console.log("On the fourth day of christmas my true love sent to me");
+        fourthDayBody();
+    }
+
+    function  fourthDayBody(){
+        console.log("Four colly birds");
+        thirdDayBody();
+    }
+
+    
+      function  fifthDay(){
+        console.log("On the fifth day of christmas my true love sent to me");
+        fifthDayBody();
+    }
+
+    function  fifthDayBody(){
+        console.log("Five golden rings");
+        fourthDayBody();
+    }
+
+
+      function  sixthDay(){
+        console.log("On the sixth day of christmas my true love sent to me");
+        sixthDayBody();
+    }
+
+    function  sixthDayBody(){
+        console.log("Six geese a-laying");
+        fifthDayBody();
+    }
+
+    
+
+      function  seventhDay(){
+        console.log("On the seventh day of christmas my true love sent to me");
+        seventhDayBody();
+    }
+
+    function  seventhDayBody(){
+        console.log("Seven swans a-swimming");
+        sixthDayBody();
+    }
+
+
+      function  eigthDay(){
+        console.log("On the eight day of christmas my true love sent to me");
+        eightDayBody();
+    }
+
+    function  eightDayBody(){
+        console.log("Eight maids a-milking");
+        seventhDayBody();
+    }
+
+
+    
+      function  ninthDay(){
+        console.log("On the eight day of christmas my true love sent to me");
+        ninthDayBody();
+    }
+
+    function  ninthDayBody(){
+        console.log("Nine ladies dancing");
+        eightDayBody();
+    }
+
+    
+    
+     function  tenthDay(){
+        console.log("On the eight day of christmas my true love sent to me");
+        tenthDayBody();
+    }
+
+    function  tenthDayBody(){
+        console.log("Ten lords a-leaping");
+        ninthDayBody();
+    }
+
+     
+     function  eleventhDay(){
+        console.log("On the eight day of christmas my true love sent to me");
+        eleventhDayBody();
+    }
+
+    function  eleventhDayBody(){
+        console.log("Eleven pipers piping");
+        tenthDayBody();
+    }
+
+
+
+    function  twelfthDay(){
+        console.log("On the eight day of christmas my true love sent to me");
+        twelfthDayBody();
+        console.log("SEE YOU NEXT TIME 🥳️");
+    }
+
+    function  twelfthDayBody(){
+        console.log("Twelve drummers drumming");
+        eleventhDayBody();
+    }
+
+    
+
+
+
+
+
+
+
+   
+
+     
+
+
+
+
+
+
+
+
+
+
