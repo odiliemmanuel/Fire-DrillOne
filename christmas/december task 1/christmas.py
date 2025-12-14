e39c67f32963bf551078b4b16f7f388e0aed35cf
Odili <odiliejeh09@gmail.com>
2025-12-14 16:10:55 +0100

    updt

--- christmas/december task 1/christmas.py
+++ christmas/december task 1/christmas.py
@@ -1,1 +1,35 @@
+from christmas_function import firstDay, secondDay, thirdDay, fourthDay, fifthDay, sixthDay, seventhDay, eightDay, ninthDay, tenthDay, eleventhDay, twelfthDay
+
+song_lyrics = ("""
+
+    1. First Day
+    2. Second Day
+    3. Third Day
+    4. Fourth Day
+    5. Fifth Day
+    6. Sixth Day
+    7. Seventh Day
+    8. Eight Day
+    9. Ninth Day
+    10. Tenth Day
+    11. Eleventh day
+    12. Twelfth Day
+
+""")
+
+print(song_lyrics)
+song = input("Enter any option")
+
+match(song):
+    case "1":
+        result = firstDay
+        print(result)
+
+
+
+
+
+
+
+
 
---
+++ christmas/december task 1/christmas_function.py
@@ -0,0 +1,144 @@
+def firstDay(days):
+    print("On the first day of christmas my true love gave to me");
+    firstDayBody(day)
+    
+def firstDayBody(day):
+    print("And a patridge in a pear tree")
+
+
+
+def secondDay(days):
+    print("On the second day of christmas my true love gave to me")
+    secondDayBody(day)
+    
+def secondDayBody(day):
+    print("Two turtle doves")
+    firstDayBody(day)
+
+
+
+def thirdDay(days):
+    print("On the third day of christmas my true love gave to me")
+    thirdDayBody(day)
+
+def thirdDayBody(day):
+    print("Three french hens")
+    secondDayBody(day)
+
+
+
+def fourthDay(days):
+    print("On the fourth day of christmas my true love gave to me")
+    fourthDayBody(day)
+
+def fourthDayBody(day):
+    print("Four colly birds")
+    thirdDayBody(day)
+
+
+
+
+def fifthDay(days):
+    print("On the fifth day of christmas my true love gave to me")
+    fifthDayBody(day)
+
+def fifthDayBody(day):
+    print("Five golden rings")
+    fourthDayBody(day)
+
+
+
+def sixthDay(days):
+    print("On the sixth day of christmas my true love gave to me")
+    sixthDayBody(day)
+
+def sixthDayBody(day):
+    print("Six geese a-laying")
+    fifthDayBody(day)
+
+
+
+def seventhDay(days):
+    print("On the seventh day of christmas my true love gave to me")
+    seventhDayBody(day)
+
+def seventhDayBody(day):
+    print("Seven swans a-swimming")
+    sixthDayBook(day)
+
+
+
+
+def eightDay(days):
+    print("On the eight day of christmas my true love gave to me")
+    eightDayBody(day)
+
+def eightDayBody(day):
+    print("Eight maids a-milking")
+    seventhDayBody(day)
+
+
+
+
+def ninthDay(days):
+    print("On the ninth day of christmas my true love gave to me")
+    ninthDayBody(day)
+
+def ninthDayBody(day):
+    print("Nine ladies dancing")
+    eightDayBody(day)
+
+
+
+
+def tenthDay(days):
+    print("On the tenth day of christmas my true love gave to me")
+    tenthDayBody(day)
+
+def tenthDayBody(day):
+    print("Ten lords a-leaping")
+    ninthDayBody(day)
+
+
+
+
+def eleventhDay(days):
+    print("On the eleventh day of christmas my true love gave to me")
+    eleventhDayBody(day)
+
+def eleventhDayBody(day):
+    print("Eleven pipers piping")
+    tenthDayBody(day)
+
+
+
+def twelfthDay(days):
+    print("On the twelfth day of christmas my true love gave to me")
+    twelfthDayBody(day)
+
+def twelfthDayBody(day):
+    print("Twelve drummers drumming")
+    eleventhDayBody(day)
+    print("SEE YOU NEXT TIME 🤪️")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
